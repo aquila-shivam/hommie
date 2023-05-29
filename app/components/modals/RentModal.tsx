48f52984994ae8329334b46e0e0e72fd89c0d11e
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2023-05-29 11:49:12 +0530

    image upload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,8 @@ import CategoryInput from "../inputs/CategoryInput"
 import { FieldValues, useForm } from "react-hook-form"
 import CountrySelect from "../inputs/CountrySelect"
 import dynamic from "next/dynamic"
+import Counter from "../inputs/Counter"
+import ImageUpload from "../inputs/ImageUpload"
 
 
 enum STEPS{
@@ -51,6 +53,10 @@ const {
 
 const category = watch('category');
 const location = watch('location');
+const guestCount = watch('guestCount')
+const roomCount = watch('roomCount');
+const bathroomCount = watch('bathroomCount');
+const imageSrc = watch('imageSrc');
 
 const Map = useMemo(()=> dynamic(()=> import('../Map'),{
   ssr:false
@@ -134,6 +140,54 @@ const secondaryActionLabel = useMemo(()=>{
           onChange={(value)=>setCustomValue('location',value)}
         />
         <Map center={location?.latlng}/>
+      </div>
+    )
+  }
+
+  if(step == STEPS.INFO)
+  {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+        title="Share some basics about your place"
+        subtitle="What amenities do you have ?"
+        />
+        <Counter 
+        title="Guests" 
+        subtitle="How many guests do you allow ?"
+        value={guestCount}
+        onChange={(value)=> setCustomValue('guestCount',value)}
+        />
+        <hr />
+        <Counter 
+        title="Rooms" 
+        subtitle="How many rooms do you have?"
+        value={roomCount}
+        onChange={(value)=> setCustomValue('roomCount',value)}
+        />
+        <hr />
+        <Counter 
+        title="Bathrooms" 
+        subtitle="How many bathroom do you have?"
+        value={bathroomCount}
+        onChange={(value)=> setCustomValue('bathroomCount',value)}
+        />
+      </div>
+    )
+  }
+
+
+  if(step === STEPS.IMAGES){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+        title="Add a photo of your place"
+        subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+        value={imageSrc}
+        onChange={(value)=>setCustomValue('imageSrc',value)}
+        />
       </div>
     )
   }
--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
   },
   images:{
     domains:
-      ["avatars.githubusercontent.com","lh3.googleusercontent.com"]
+      [
+      "avatars.githubusercontent.com"
+      ,"lh3.googleusercontent.com",
+      "res.cloudinary.com"
+      ]
   }
 }
 
